ecb48ecb563c394b906187f86777a3caeaf2a87c
Akshat Asati <asatiakshat7@gmail.com>
2024-06-11 17:47:39 +0530

    Home page components

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../features/auth/components/Login'
+
+
+function LoginPages() {
+    return (
+        <>
+        <Login></Login> 
+        </>
+    )
+}
+
+export default LoginPages
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signup from '../features/auth/components/Signup'
+
+function SignupPages() {
+    return (
+        <Signup></Signup>
+    )
+}
+
+export default SignupPages
